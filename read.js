35970f52036a439d464c17ada53b21123edb5482
vicnatacu <vicki@mvbmb.com>
2019-10-17 16:25:23 +1000

    initial setup

---
+++ read.js
@@ -0,0 +1,12 @@
+function readFunction(idName, btnId) {
+    meText = document.getElementById(idName);
+    btn = document.getElementById(btnId);
+    if(btn.innerHTML == "more") {
+      meText.classList.remove("hide-me");
+      btn.innerHTML = "less";
+    } else {
+      meText.classList.add("hide-me");
+      btn.innerHTML = "more";
+    }
+
+  }
